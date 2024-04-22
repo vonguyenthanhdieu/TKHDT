36d6ec9437c0a5a6bc312476ae9e8b9954c0b77f
vonguyenthanhdieu <145991225+vonguyenthanhdieu@users.noreply.github.com>
2024-04-23 00:02:34 +0700

    Add files via upload

---
+++ daoluongduy-gmail.com-master/src/controller/Main.java
@@ -0,0 +1,11 @@
+package controller;
+
+public class Main {
+        MainFrame frame;
+        public Main() {
+		frame = new MainFrame();
+	}
+        public static void main(String[] args) {
+		new Main();
+	}
+}
